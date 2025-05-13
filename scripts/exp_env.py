2b52042f753ece6a1ad64f1f85a0a129cda22914
phw <arcturus812@gmail.com>
2025-05-13 10:25:38 +0900

    fix untracked files

--- scripts/exp_env.py
+++
@@ -1,6 +0,0 @@
-VENV_PATH="/home/phw/workspace/.venv"
-
-SIM_ROOT="/home/phw/workspace/simulator/ChampSim_phw"
-SIM_BIN=SIM_ROOT + "/bin/champsim"
-TRACE_ROOT="/home/phw/workspace/traces/Champsim"
-LOG_ROOT="/home/phw/workspace/simulator/logs/lastest_logs"
